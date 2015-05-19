7c0da5fbc50c8f73da45657f3fbd701e538df393
Mike Pitre <[michaelepitre@gmail.com]>
2015-05-19 18:51:07 -0500

    added 1 player mode, computer chooses available positions

--- lib/board.rb
+++ lib/board.rb
@@ -30,23 +30,23 @@ class Board
   def player1_selection
     puts "Player1, what position would you like to take?"
     take_selection = gets.chomp.to_i
-      if take_selection == 1 #&& (@space1 != "X" || "O")
+      if (take_selection == 1) && (@space1 == 1)
         @space1 = "X"
-      elsif take_selection == 2
+      elsif (take_selection == 2) && (@space2 == 2)
         @space2 = "X"
-      elsif take_selection == 3
+      elsif (take_selection == 3) && (@space3 == 3)
         @space3 = "X"
-      elsif take_selection == 4
+      elsif (take_selection == 4) && (@space4 == 4)
         @space4 = "X"
-      elsif take_selection == 5
+      elsif (take_selection == 5) && (@space5 == 5)
         @space5 = "X"
-      elsif take_selection == 6
+      elsif (take_selection == 6) && (@space6 == 6)
         @space6 = "X"
-      elsif take_selection == 7
+      elsif (take_selection == 7) && (@space7 == 7)
         @space7 = "X"
-      elsif take_selection == 8
+      elsif (take_selection == 8) && (@space8 == 8)
         @space8 = "X"
-      elsif take_selection == 9
+      elsif (take_selection == 9) && (@space9 == 9)
         @space9 = "X"
       else
         puts "Not a valid selection, please choose an available position 1-9. > "
@@ -59,23 +59,23 @@ class Board
   def player2_selection
     puts "Player2, what position would you like to take?"
     take_selection = gets.chomp.to_i
-    if take_selection == 1
+    if (take_selection == 1) && (@space1 == 1)
       @space1 = "O"
-    elsif take_selection == 2
+    elsif (take_selection == 2) && (@space2 == 2)
       @space2 = "O"
-    elsif take_selection == 3
+    elsif (take_selection == 3) && (@space3 == 3)
       @space3 = "O"
-    elsif take_selection == 4
+    elsif (take_selection == 4) && (@space4 == 4)
       @space4 = "O"
-    elsif take_selection == 5
+    elsif (take_selection == 5) && (@space5 == 5)
       @space5 = "O"
-    elsif take_selection == 6
+    elsif (take_selection == 6) && (@space6 == 6)
       @space6 = "O"
-    elsif take_selection == 7
+    elsif (take_selection == 7) && (@space7 == 7)
       @space7 = "O"
-    elsif take_selection == 8
+    elsif (take_selection == 8) && (@space8 == 8)
       @space8 = "O"
-    elsif take_selection == 9
+    elsif (take_selection == 9) && (@space9 == 9)
       @space9 = "O"
     else
         puts "Not a valid selection, please choose an available position 1-9. > "
@@ -85,59 +85,88 @@ class Board
     @moves_counter += 1
   end
 
-    def win?
-      if @space1 == "X" && @space2 == "X" && @space3 == "X"
-        puts "Player 1 wins!"
-        return true
-      elsif @space4 == "X" && @space5 == "X" && @space6 == "X"
-        puts "Player 1 wins!"
-        return true
-      elsif @space7 == "X" && @space8 == "X" && @space9 == "X"
-        puts "Player 1 wins!"
-        return true
-      elsif @space1 == "X" && @space5 == "X" && @space9 == "X"
-        puts "Player 1 wins!"
-        return true
-      elsif @space3 == "X" && @space5 == "X" && @space7 == "X"
-        puts "Player 1 wins!"
-        return true
-      elsif @space1 == "X" && @space4 == "X" && @space7 == "X"
-        puts "Player 1 wins!"
-        return true
-      elsif @space2 == "X" && @space5 == "X" && @space8 == "X"
-        puts "Player 1 wins!"
-        return true
-      elsif @space3 == "X" && @space6 == "X" && @space9 == "X"
-        puts "Player 1 wins!"
-        return true
-      elsif @space1 == "O" && @space2 == "O" && @space3 == "O"
-        puts "Player 2 wins!"
-        return true
-      elsif @space4 == "O" && @space5 == "O" && @space6 == "O"
-        puts "Player 2 wins!"
-        return true
-      elsif @space7 == "O" && @space8 == "O" && @space9 == "O"
-        puts "Player 2 wins!"
-        return true
-      elsif @space1 == "O" && @space5 == "O" && @space9 == "O"
-        puts "Player 2 wins!"
-        return true
-      elsif @space3 == "O" && @space5 == "O" && @space7 == "O"
-        puts "Player 2 wins!"
-        return true
-      elsif @space1 == "O" && @space4 == "O" && @space7 == "O"
-        puts "Player 2 wins!"
-        return true
-      elsif @space2 == "O" && @space5 == "O" && @space8 == "O"
-        puts "Player 2 wins!"
-        return true
-      elsif @space3 == "O" && @space6 == "O" && @space9 == "O"
-        puts "Player 2 wins!"
-        return true
-      elsif @moves_counter == 9
-        puts "It's a draw! Nobody wins!"
-        return true
-      end
+  def computer_selection
+    take_selection = rand(1..9)
+    if (take_selection == 1) && (@space1 == 1)
+      @space1 = "O"
+    elsif (take_selection == 2) && (@space2 == 2)
+      @space2 = "O"
+    elsif (take_selection == 3) && (@space3 == 3)
+      @space3 = "O"
+    elsif (take_selection == 4) && (@space4 == 4)
+      @space4 = "O"
+    elsif (take_selection == 5) && (@space5 == 5)
+      @space5 = "O"
+    elsif (take_selection == 6) && (@space6 == 6)
+      @space6 = "O"
+    elsif (take_selection == 7) && (@space7 == 7)
+      @space7 = "O"
+    elsif (take_selection == 8) && (@space8 == 8)
+      @space8 = "O"
+    elsif (take_selection == 9) && (@space9 == 9)
+      @space9 = "O"
+    else
+        computer_selection
+        @moves_counter -=1
     end
+    @moves_counter += 1
+  end
+
+
+
+  def win?
+    if @space1 == "X" && @space2 == "X" && @space3 == "X"
+      puts "Player 1 wins!"
+      return true
+    elsif @space4 == "X" && @space5 == "X" && @space6 == "X"
+      puts "Player 1 wins!"
+      return true
+    elsif @space7 == "X" && @space8 == "X" && @space9 == "X"
+      puts "Player 1 wins!"
+      return true
+    elsif @space1 == "X" && @space5 == "X" && @space9 == "X"
+      puts "Player 1 wins!"
+      return true
+    elsif @space3 == "X" && @space5 == "X" && @space7 == "X"
+      puts "Player 1 wins!"
+      return true
+    elsif @space1 == "X" && @space4 == "X" && @space7 == "X"
+      puts "Player 1 wins!"
+      return true
+    elsif @space2 == "X" && @space5 == "X" && @space8 == "X"
+      puts "Player 1 wins!"
+      return true
+    elsif @space3 == "X" && @space6 == "X" && @space9 == "X"
+      puts "Player 1 wins!"
+      return true
+    elsif @space1 == "O" && @space2 == "O" && @space3 == "O"
+      puts "Player 2 wins!"
+      return true
+    elsif @space4 == "O" && @space5 == "O" && @space6 == "O"
+      puts "Player 2 wins!"
+      return true
+    elsif @space7 == "O" && @space8 == "O" && @space9 == "O"
+      puts "Player 2 wins!"
+      return true
+    elsif @space1 == "O" && @space5 == "O" && @space9 == "O"
+      puts "Player 2 wins!"
+      return true
+    elsif @space3 == "O" && @space5 == "O" && @space7 == "O"
+      puts "Player 2 wins!"
+      return true
+    elsif @space1 == "O" && @space4 == "O" && @space7 == "O"
+      puts "Player 2 wins!"
+      return true
+    elsif @space2 == "O" && @space5 == "O" && @space8 == "O"
+      puts "Player 2 wins!"
+      return true
+    elsif @space3 == "O" && @space6 == "O" && @space9 == "O"
+      puts "Player 2 wins!"
+      return true
+    elsif @moves_counter == 9
+      puts "It's a draw! Nobody wins!"
+      return true
+    end
+  end
 end
 
--- lib/game.rb
+++ lib/game.rb
@@ -9,17 +9,36 @@ class Game
   def start
     puts "Let's play tic-tac-toe!"
     @board.display_board
-    puts "Player1 is X, Player 2 is O"
-    while @board.win? == nil
-      @board.player1_selection
-      @board.display_board
-      if @board.win? == true
-        play_again?
+    puts "How many players? (1 or 2) > "
+    number_of_players = gets.chomp.to_i
+    if number_of_players == 2
+      puts "Player1 is X, Player 2 is O"
+      while @board.win? == nil
+        @board.player1_selection
+        @board.display_board
+        if @board.win? == true
+          play_again?
+        end
+        @board.player2_selection
+        @board.display_board
+        if @board.win? == true
+          play_again?
+        end
       end
-      @board.player2_selection
-      @board.display_board
-      if @board.win? == true
-        play_again?
+    end
+    if number_of_players == 1
+      puts "Player1 is X, Player 2 (Computer) is O"
+      while @board.win? == nil
+        @board.player1_selection
+        @board.display_board
+        if @board.win? == true
+          play_again?
+        end
+        @board.computer_selection
+        @board.display_board
+        if @board.win? == true
+          play_again?
+        end
       end
     end
   end
